004ab0fd823efe7693f991fe1b3e5ff38c554a52
Einenlum <einenlum@vmail.me>
2015-04-22 16:42:10 +0200

    Add Twig extensions

---
+++ src/Knp/Rad/AutoRegistration/DefinitionBuilder/TwigExtensionBuilder.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace Knp\Rad\AutoRegistration\DefinitionBuilder;
+
+use Knp\Rad\AutoRegistration\DefinitionBuilder;
+use Knp\Rad\AutoRegistration\Kernel\KernelWrapper;
+use Knp\Rad\AutoRegistration\Finder\BundleFinder;
+use Knp\Rad\AutoRegistration\Reflection\ClassAnalyzer;
+use Symfony\Component\DependencyInjection\Definition;
+
+class TwigExtensionBuilder implements DefinitionBuilder
+{
+    /**
+     * @var KernelWrapper
+     */
+    private $kernel;
+
+    /**
+     * @var BundleFinder
+     */
+    private $finder;
+
+    /**
+     * @var ClassAnalyzer
+     */
+    private $analyzer;
+
+    /**
+     * @param KernelWrapper $kernel
+     * @param BundleFinder  $finder
+     * @param ClassAnalyzer $analyzer
+     */
+    public function __construct(KernelWrapper $kernel, BundleFinder $finder, ClassAnalyzer $analyzer)
+    {
+        $this->kernel   = $kernel;
+        $this->finder   = $finder;
+        $this->analyzer = $analyzer;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function buildDefinitions()
+    {
+        $definitions = [];
+
+        $twigExtensions = $this->finder->findClasses(
+            $this->kernel->getBundles(),
+            ['Twig', 'Templating'],
+            'TwigExtension'
+        );
+
+        foreach ($twigExtensions as $twigExtension) {
+            if (true === $this->analyzer->needConstruction($twigExtension)) {
+                continue;
+            }
+
+            $definitions[$twigExtension] = (new Definition())
+                ->setClass($twigExtension)
+                ->addTag('twig.extension')
+            ;
+        }
+
+        return $definitions;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return 'twig_extension';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function isActive()
+    {
+        return true;
+    }
+}
